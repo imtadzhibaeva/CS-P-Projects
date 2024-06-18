46aa27bb906b9161649e931331ea9daea029c6ef
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2024-06-17 22:30:42 -0400

    lexer

--- ProjectP2/src/Lexer.java
+++ ProjectP2/src/Lexer.java
@@ -1,5 +1,27 @@
 public class Lexer {
     
 
-    public Lexer(String input) {}
+    public Lexer(String input) {
+        this.tokens = new ArrayList<Token>();
+        tokenize();
+    }
+
+    private void tokenize() {
+
+    }
+
+    class Token{
+final TokenType type;
+final String value;
+
+Token(TokenType type, String value) {
+    this.type = type;
+    this.value = value;
+}
+    }
+
+    enum TokenType{
+        CONFIG, UPDATE, COMPUTE, SHOW, CONFIGS, STRING, NUMBERS, IDENTIFIEFR, REFERENCES
+    }
+
 }
