86b28852b670e478f0c6324b64b08f1264563680
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2024-06-24 11:42:53 -0400

    SCanner added

--- ProjectP2/src/Calculator.java
+++ ProjectP2/src/Calculator.java
@@ -1,7 +1,21 @@
 
+//ADD 34 3
+//SUB 31 2
+
+import java.util.Scanner;
 
 public class Calculator {
   public static void main(String[] args) {
     System.out.println("H1");
+
+    Scanner scanner = new Scanner(System.in);
+
+    String raw_expr = scanner.nextLine();
+    String[] data = raw_expr.split(" ");
+    if (data.length != 3 ) {
+      throw new IllegalArgumentException("Input doesnt satisfy language rules: " + raw_expr);
+    }
+
+    scanner.close();
   }  
 }
